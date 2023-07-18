299aef809a1fcf039055f67ee7a01f7131d62438
nicole-gallo <139741919+nicole-gallo@users.noreply.github.com>
2023-07-18 17:47:49 +0800

    FIRST FINAL

--- src/components/content.js
+++ src/components/content.js
@@ -1,13 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const App = () => {
+  return "Hello world";
+};
 
+export default App;
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(<App />);
